eb4316eb4b620c8a97349e54871368d3e11a38b5
Pavel Brichkin <pavlikbrichkin11@gmail.com>
2022-01-15 02:46:48 +0500

    Completed app

--- app/src/main/java/com/example/natashacats/MainActivity.java
+++ app/src/main/java/com/example/natashacats/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.natashacats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView secondText = (TextView) findViewById(R.id.txt2);
+        TextView thirdText = (TextView) findViewById(R.id.txt3);
+        TextView forthText = (TextView) findViewById(R.id.txt4);
+        ImageView cat = (ImageView) findViewById(R.id.imageView4);
+
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phrases[] = {"Они щас все\nЕве сломают!",
+                        "Настя уже без\nмузыки танцует!!!",
+                        "Данэль не справляется\nс Настей!!",
+                        "Еве стало плохо",
+                        "Они морду потеряли!!!" };
+
+                Random random = new Random();
+                secondText.setText(phrases[random.nextInt(4)]);
+                thirdText.setText(phrases[random.nextInt(4)]);
+                forthText.setText(phrases[random.nextInt(4)]);
+            }
+        });
+
     }
 
 
